ee6334d9e11d58986f09a01c2423c3ad05d7e67d
BasobPragyan <pragyan95prabodhbanerjee@gmail.com>
2024-10-18 12:47:45 +0530

    mirror triangle

---
+++ project.py
@@ -0,0 +1,16 @@
+print("Right angle Triangle")
+n=int(input("Enter no. of rows : "))
+for i in range(n):
+    for j in range(i+1):
+     print("codingal ",end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+     print("codingal ",end=" ")
+while(i > 0):
+    j = i
+    while(j > 0):
+        print("codingal ",end = " ")
+        j = j-1
+        i = i-1
+print()
